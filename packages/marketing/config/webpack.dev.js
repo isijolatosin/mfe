e94258bc0135d48bf1cce44b8c136fd767e0cd8e
isijolatosin <isijolatosin2210@gmail.com>
2025-04-04 20:47:36 -0400

    first commit

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/moduleFederationPlugin");
+const packageJson = require("../package.json");
+const commonConfig = require("./webpack.common");
+
+const devConfig = {
+	mode: "development",
+	devServer: {
+		port: 8081,
+		historyApiFallback: {
+			index: "index.html",
+		},
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "marketing",
+			filename: "remoteEntry.js",
+			exposes: {
+				"./MarketingApp": "./src/bootstrap",
+			},
+			shared: packageJson.dependencies,
+		}),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "../public/index.html"),
+		}),
+	],
+};
+
+module.exports = merge(commonConfig, devConfig);
